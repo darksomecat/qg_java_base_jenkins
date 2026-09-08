ce191061f3178589036271084f25befc7ad208b9
Anna Ukolova <anikadd3@gmail.com>
2026-09-08 23:20:33 +0300

    update allure version in gradle

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -12,6 +12,8 @@ public class TestBase {
         Configuration.baseUrl = "https://qa-guru.github.io/one-page-form";
         //Configuration.holdBrowserOpen = true;
         Configuration.browserSize="1428x1158";
+//        Configuration.browserVersion = "128.0";
+        Configuration.remote = "https://user1:1234@selenoid.qa.guru/wd/hub";
         closeWebDriver();
     }
 }
